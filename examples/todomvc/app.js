e4b06ce2c13b7a11864d9c75b43b28575c9c06c4
Artemi Krymski <artemi@krymski.com>
2014-11-10 14:22:31 +0000

    todomvc

--- examples/todomvc/app.js
+++ examples/todomvc/app.js
@@ -5,19 +5,24 @@ var Collection  = Espresso.Collection
 var Controller  = Espresso.Controller
 var List        = Espresso.List
 var extend      = Espresso.extend
-var $           = function(x) { return document.getElementById(x) }
 var ENTER_KEY   = 13;
 var ESC_KEY     = 27;
 
-var ToDoCollection = extend(Collection, {
+var ToDoStore = extend(Collection, {
     init: function() {
         this.clearCompleted = this.clearCompleted.bind(this);
+        this.add = this.add.bind(this);
         this.reset(JSON.parse(localStorage.getItem('todo')));
-    },   
+        this.addListener('change', this.save.bind(this));
+    },
+    add: function(txt) {
+        this.push({ done: false, id: this.count(), text: txt })
+    },
     toggle: function(id, done) {
-        var index = this.index(id);
-        var item = this.get(index);
-        this.set(index, { done: done, id: item.id, text: item.text })
+        // var index = this.findIndex({ id: id });
+        // var item = this.get(index);
+        // this.set(index, { done: done, id: item.id, text: item.text })
+        this.set({ id: id, done: done })
     },
     toggleAll: function(done) {
         this.forEach(function(v, i) {
@@ -27,16 +32,16 @@ var ToDoCollection = extend(Collection, {
         }.bind(this))
     },
     clearCompleted: function() {
-        var done = [];
-        this.forEach(function(v, i) {
-            if (v.done) done.push(i)
-        }.bind(this))
-        done.forEach(this.remove.bind(this))
+        this.set(this.active());
     },
     completed: function() {
-        var completed = 0;
-        this.forEach(function(v) { if (v.done) completed++ }.bind(this));
-        return completed;
+        return this.filter(function(v) { return v.done });
+    },
+    active: function() {
+        return this.filter(function(v) { return !v.done });
+    },
+    all: function() {
+        return this.toArray();
     },
     save: function() {
         localStorage.setItem('todo', JSON.stringify(this.toArray()));
@@ -56,7 +61,7 @@ var ToDoItem = extend(Controller, {
         this.set({ editing: true });
     },
     destroy: function() {
-        items.remove({ id: this.model.id });
+        store.remove({ id: this.model.id });
     },
     key: function(e) {
         if (e.which === ENTER_KEY) this.set({ editing: false, text: e.target.value });
@@ -66,7 +71,7 @@ var ToDoItem = extend(Controller, {
         }
     },
     toggle: function(e) {
-        items.toggle(this.model.id, e.target.checked);
+        store.toggle(this.model.id, e.target.checked);
     },
     render: function() {
         return {
@@ -81,51 +86,48 @@ var ToDoItem = extend(Controller, {
 
 var App = extend(Controller, {
     init: function() {
-        this.model.count = 0;
-        this.list = new List(ToDoItem, []);
-        this.listenTo(items, 'change', function() {
-            this.set({ count: items.count() - items.completed() });
-            items.save();
-        });
-        this.listenTo($('filters'), 'click', function(e) {
-            if (e.target.nodeName === 'A') this.filter(e.target.innerHTML.toLowerCase())
-        });
-        this.filter('all');
+        this.model.filter = window.location.hash.replace('#/', '') || 'all';
+        if (this.model.filter === 'active') this.filter({ target: this.ref.active });
+        if (this.model.filter === 'completed') this.filter({ target: this.ref.completed });
+
+        this.list = new List(ToDoItem);
+        this.listenTo(store, 'change', this.render);
     },
-    filter: function(name) {
-        var filtered = items.filter(function(v) {
-            if (name === 'completed' && !v.done) return false;
-            if (name === 'active' && v.done) return false;
-            return true;
-        });
-        this.list.set(filtered);
+    filter: function(e) {
+        if (e === undefined) return 
+        if (e.target.nodeName === 'A') {
+            document.querySelector('a.selected').classList.remove('selected');
+            e.target.classList.add('selected')
+            this.model.set({ filter: e.target.innerHTML.toLowerCase() })
+        }
     },
     addItem: function(e) {
         if (e.which !== ENTER_KEY) return;
-        items.push({ done:false, text:this.ref.newItem.value, id: items.count() })
+        store.add(this.ref.newItem.value)
         this.ref.newItem.value = '';
     },
     toggleAll: function(e) {
-        items.toggleAll(e.target.checked);
+        store.toggleAll(e.target.checked);
     },
     clearText: function() {
-        var x = items.count() - this.model.count
+        var x = store.completed().length;
         return 'Clear completed (' + x + ')'
     },
     render: function() {
         return {
-            footer: { display: items.count() > 0 },
-            clear: { text: this.clearText(), onclick: items.clearCompleted, display: this.model.count < items.count() },
-            list: this.list,
+            list: this.list.set(store[this.model.filter]()),
+            filters: { onclick: this.filter },
+            footer: { display: store.count() > 0 },
             toggleAll: { onclick: this.toggleAll },
             newItem: { onkeypress: this.addItem },
-            count: { html: '<strong>'+this.model.count+'</strong> items left' }
+            count: { html: '<strong>'+store.active().length+'</strong> items left' },
+            clear: { text: this.clearText(), onclick: store.clearCompleted, display: store.completed().length > 0 }
         }
     }
 });
 
-var items = new ToDoCollection([
+var store = new ToDoStore([
   // { id: 0, text: 'write todomvc', done: false }
 ]);
 
-window.app = new App({ view: $('todoapp') });
+window.app = new App({ view: document.getElementById('todoapp') });
